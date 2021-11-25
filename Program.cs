3831fe15ff9611a1624a0af52cb44f5efb5f4afa
haliltopcuoglu <60238362+haliltopcuoglu@users.noreply.github.com>
2021-11-25 13:18:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsmini Gir");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soy İsmini Gir");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
